da70a57179a58ba5158c96357f811df85f1b09c6
Avery Pennarun <apenwarr@gmail.com>
2009-09-01 20:46:34 -0400

    Add a unix socket listener.
    Of course, naming it /tmp/socky wasn't such a great idea.

--- jfauthd.cc
+++ jfauthd.cc
@@ -1,6 +1,7 @@
 #include "jfauthd.h"
 #include <wvstreams/wvstreamsdaemon.h>
 #include <wvstreams/wvtcp.h>
+#include <wvstreams/wvunixsocket.h>
 #include <wvstreams/wvstreamclone.h>
 #include <stdio.h>
 
@@ -22,10 +23,11 @@ public:
 	else
 	{
 	    read(buf, 1024);
-	    if (buf.strchr(0))
+	    int ofs = buf.strchr(0);
+	    if (ofs)
 	    {
 		WvStringList l;
-		l.split(buf.getstr(), "\r\n");
+		l.split(buf.getstr(ofs), "\r\n");
 		int ver = l.popstr().num();
 		if (ver != 1)
 		{
@@ -38,9 +40,10 @@ public:
 		
 		WvString user = l.popstr();
 		WvString pass = l.popstr();
-		printf("ver:%d user:'%s' pass:'%s'\n",
-		       ver, user.cstr(), pass.cstr());
-		WvError e = jfauth_pam("jfauthd", "tcp", user, pass);
+		printf("ver:%d user:'%s' pass:'%s' (src='%s')\n",
+		       ver, user.cstr(), pass.cstr(),
+		       WvString(*src()).cstr());
+		WvError e = jfauth_pam("jfauthd", *src(), user, pass);
 		print("%s\r\n%s\r\n", e.get(), e.str());
 		close();
 	    }
@@ -54,20 +57,33 @@ public:
 };
 
 
-static void incoming(WvStream &, void *userdata)
+static void tcp_incoming(WvStream &, void *userdata)
 {
-    printf("incoming\n");
+    printf("tcp incoming\n");
     WvTCPListener *l = (WvTCPListener *)userdata;
     WvIStreamList::globallist.append(new AuthStream(l->accept()),
-				     true, (char *)"incoming");
+				     true, (char *)"tcp_incoming");
+}
+
+
+static void unix_incoming(WvStream &, void *userdata)
+{
+    printf("unix incoming\n");
+    WvUnixListener *l = (WvUnixListener *)userdata;
+    WvIStreamList::globallist.append(new AuthStream(l->accept()),
+				     true, (char *)"unix_incoming");
 }
 
 
 static void startup(WvStreamsDaemon &daemon, void *)
 {
     WvTCPListener *l = new WvTCPListener(5478);
-    l->setcallback(incoming, l);
+    l->setcallback(tcp_incoming, l);
     daemon.add_die_stream(l, true, (char *)"tcplistener");
+    
+    WvUnixListener *l2 = new WvUnixListener("/tmp/socky", 0666);
+    l2->setcallback(unix_incoming, l2);
+    daemon.add_die_stream(l2, true, (char *)"unixlistener");
 }
 
 
